620953d17e2aaffcc6c699c6afb5ede93eac3e1d
Arun Yadav <yarun4622@gmail.com>
2024-09-27 15:45:02 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -3,7 +3,7 @@ def romanToInt(s: str) -> int:
         'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000
     }
     
-    # Initialize total to 0
+   
     total = 0
     
     # Traverse through the Roman numeral string
