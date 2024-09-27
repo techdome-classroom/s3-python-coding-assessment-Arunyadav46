b63442821d525977ffef507b19c1219f1e6aab6d
Arun Yadav <yarun4622@gmail.com>
2024-09-27 15:38:18 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -6,5 +6,34 @@ class Solution(object):
         """
         pass
 
+        def romanToInt(s: str) -> int:
+    # Mapping of Roman numerals to their respective integer values
+    roman_map = {
+        'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000
+    }
+    
+    # Initialize total to 0
+    total = 0
+    
+    # Traverse through the Roman numeral string
+    for i in range(len(s)):
+        # If the current value is less than the next one, subtract it
+        if i + 1 < len(s) and roman_map[s[i]] < roman_map[s[i + 1]]:
+            total -= roman_map[s[i]]
+        else:
+            # Otherwise, add it to the total
+            total += roman_map[s[i]]
+    
+    return total
+
+# Example usage:
+s1 = "III"
+s2 = "LVIII"
+s3 = "MCMXCIV"
+
+print(romanToInt(s1))  # Output: 3
+print(romanToInt(s2))  # Output: 58
+print(romanToInt(s3))  # Output: 1994
+
 
 
